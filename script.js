2390e673106995bb0f938892af4b5098edbe1965
Lukman <50276748+lukman-s@users.noreply.github.com>
2020-03-27 01:50:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const myName = 'Lukman Sanni';
+
+const courses = ['Html', 'Css', 'Javascript'];
+
+console.log('Name: ' + myName +'.');
+console.log('Courses: ' + courses[0] +', '+ courses[1] + ', ' + courses[2] + '.')
+for (let i=1; i<=200; i++) {
+    if(i%2 !== 0) {
+        console.log(i);
+    }
+}
